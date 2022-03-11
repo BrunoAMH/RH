4f98affb1ee9d7ee22793543e6e5c4fc43299bb6
XLuis-Eduardo-Espinoza-Mares <86754926+XLuis-Eduardo-Espinoza-Mares@users.noreply.github.com>
2022-03-10 21:41:17 -0600

    Ciudad Dao

--- model/DAO.py
+++ model/DAO.py
@@ -85,3 +85,29 @@ class Turnos(db.Model):
         obj = self.consultaIndividual(id)
         db.session.delete(obj)
         db.session.commit()
+        
+ class Ciudades(db.Model):
+    __tablename__ = 'ciudades'
+    idCiudad = Column(Integer, primary_key=True)
+    nombre = Column(String(60), nullable=False)
+    idEstado = Column(Integer,ForeignKey('estados.idEstado'))
+    estatus = Column(CHAR(1), nullable=False)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividual(self, id):
+        return self.query.get(id)
+
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self, id):
+        obj = self.consultaIndividual(id)
+        db.session.delete(obj)
+        db.session.commit()
