1fa5941b4434672eef70218ebc5a196838aad508
BrunoAMH <86796020+BrunoAMH@users.noreply.github.com>
2022-03-13 12:49:58 -0600

    Update DAO.py

--- model/DAO.py
+++ model/DAO.py
@@ -58,7 +58,7 @@ class Puestos(db.Model):
         obj = self.consultaIndividual(id)
         db.session.delete(obj)
         db.session.commit()
-
+        
 class Turnos(db.Model):
     __tablename__ = 'turnos'
     idTurno = Column(Integer, primary_key=True)
@@ -85,6 +85,111 @@ class Turnos(db.Model):
         obj = self.consultaIndividual(id)
         db.session.delete(obj)
         db.session.commit()
+ 
+class Percepciones(db.Model):
+    __tablename__ = 'percepciones'
+    idPercepcion = Column(Integer, primary_key=True)
+    nombre = Column(String(30), nullable=False)
+    descripcion = Column(String(80), nullable=False)
+    diasPagar = Column(Integer, nullable=False)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividual(self, id):
+        return self.query.get(id)
+
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self, id):
+        obj = self.consultaIndividual(id)
+        db.session.delete(obj)
+        db.session.commit()
+
+class Deducciones(db.Model):
+    __tablename__ = 'deducciones'
+    idDeduccion = Column(Integer, primary_key=True)
+    nombre = Column(String(30), nullable=False)
+    descripcion = Column(String(80), nullable=False)
+    porcentaje = Column(Float, nullable=False)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividual(self, id):
+        return self.query.get(id)
+
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self, id):
+        obj = self.consultaIndividual(id)
+        db.session.delete(obj)
+        db.session.commit()
+
+class Periodos(db.Model):
+    __tablename__ = 'periodos'
+    idPeriodo = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    fechaInicio = Column(Date, nullable=False)
+    fechaFin = Column(Date, nullable=False)
+    estatus = Column(CHAR(1), nullable=False)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividual(self, id):
+        return self.query.get(id)
+
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self, id):
+        obj = self.consultaIndividual(id)
+        db.session.delete(obj)
+        db.session.commit()
+
+class FormasPago(db.Model):
+    __tablename__ = 'formaspago'
+    idFormaPago = Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
+    estatus = Column(CHAR(1), nullable=False)
+
+    def consultaGeneral(self):
+        return self.query.all()
+
+    def consultaIndividual(self, id):
+        return self.query.get(id)
+
+    def insertar(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def actualizar(self):
+        db.session.merge(self)
+        db.session.commit()
+
+    def eliminar(self, id):
+        obj = self.consultaIndividual(id)
+        db.session.delete(obj)
+        db.session.commit()
+
         
  class Ciudades(db.Model):
     __tablename__ = 'ciudades'
